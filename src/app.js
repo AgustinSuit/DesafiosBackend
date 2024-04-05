d79c2e48c2e0f42dbd4b70fd6cedd54f943d2e79
Agustin Bracamonte <AgustinPorato@hotmail.com>
2024-04-04 21:47:12 -0300

    Handlebars Added

--- src/app.js
+++ src/app.js
@@ -5,8 +5,36 @@ import cartRouter from "../src/routes/cartRoutes.js";
 const app = express();
 const PORT = 8080;
 
+//Middleware
+app.use(express.static("./src/public"));
+
+import fs from 'fs';
+
+const obtenerProductosDesdeArchivo = () => {
+    try {
+        const data = fs.readFileSync('./src/db/productos.json', 'utf-8');
+        const productos = JSON.parse(data);
+        return productos;
+    } catch (error) {
+        console.error('Error al leer el archivo de productos:', error);
+        return [];
+    }
+};
+
+
+// Configurando HandleBars
+import exphbs from "express-handlebars";
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
 // Middleware para parsear el body como JSON
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    const products = obtenerProductosDesdeArchivo();
+    res.render("home", { products, titulo: "Inicio" });
+})
 
 app.use('/api/carts', cartRouter);
 
