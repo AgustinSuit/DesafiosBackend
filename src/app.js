ed5ac7e526ecc4cc4efb4557364ea8be1865a2a5
Agustin Bracamonte <AgustinPorato@hotmail.com>
2024-04-10 16:39:17 -0300

    Agrego WebSockets

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import productRouter from "../src/routes/productRoutes.js";
 import cartRouter from "../src/routes/cartRoutes.js";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = 8080;
@@ -36,13 +37,45 @@ app.get("/", (req, res) => {
     res.render("home", { products, titulo: "Inicio" });
 })
 
+app.get("/realtimeproducts", async (req, res) => {
+    res.render("realTimeProducts")
+})
+
+
 app.use('/api/carts', cartRouter);
 
 app.use('/api/products', productRouter);
 
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
     console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
+
+// Configurando Socket.io
+const io = new Server(httpServer);
+
+// Array de productos:
+
+import ProductManager from './managers/productManager.js';
+const productManager = new ProductManager("./src/db/productos.json");
+
+io.on("connection", async (socket) => {
+    console.log("Cliente conectado");
+
+    // Array de productos enviar:
+    socket.emit("productos", await productManager.getProducts());
+
+    // Recibimos el evento "eliminarProducto" desde el cliente:
+    socket.on("eliminarProducto", async (id) => {
+        await productManager.deleteProduct(id);
+        // Enviamos el array de productos actualizado
+        socket.emit("productos", await productManager.getProducts());
+    })
+    // Recibimos el evento "agregarProducto" desde el cliente:
+    socket.on("agregarProducto", async (producto) => {
+        await productManager.addProduct(producto);
+        socket.emit("productos", await productManager.getProducts());
+    })
+})
 
 export default app;
 
